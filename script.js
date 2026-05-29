67f2c7404e1a6406efd7e816c49e546fb4f8e4e6
Andrey Nikitin <a_nik74@mail.ru>
2026-05-29 20:00:18 +0300

    c

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let elem = document.querySelector(".left")
+// console.log(elem.outerHTML) 
+console.log(elem.innerHTML)
+console.log("+".repeat(40))
+console.log(elem.innerText)
+console.log("+".repeat(40))
+console.log(elem.textContent)
+
